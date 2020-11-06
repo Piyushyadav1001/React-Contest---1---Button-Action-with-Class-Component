50d4fd5f2a563cb283de3eace84d95d49251d124
Piyushyadav1001 <72632167+Piyushyadav1001@users.noreply.github.com>
2020-11-06 21:39:40 +0530

    Changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,25 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "./../styles/App.css";
 
-class App extends Component {
-    constructor(props) {
-		super(props);
-	};
+function App() {
+  const [clicks, setValue] = useState(false);
 
-    render() {
-    	return(
-    		<div id="main">
-				{ /* Do not remove this main div!! */ }
-    		</div>
-    	);
-    }
+  function onButtonClick() {
+    setValue(true);
+  }
+  return (
+    <div id="main">
+      <button id="click" onClick={onButtonClick}>
+        click
+      </button>
+      {clicks ? (
+        <p id="para">
+          Hello, I've learnt to use the full-stack evaluation tool. This makes
+          me so happy
+        </p>
+      ) : null}
+    </div>
+  );
 }
 
-
 export default App;
-
